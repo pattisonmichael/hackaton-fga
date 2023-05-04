4b0c4616603473d940c29f8039b02ad9ed83d5fe
Michael Pattison <michael.pattison@auth0.com>
2023-05-04 07:24:32 +0200

    cleanup

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ configuration = openfga_sdk.Configuration(
     credentials = credentials,
     store_id = "01GZE886BK7NC959XNE0DJ7PZE",
 )
-configuration.debug=True
+#configuration.debug=True
 
 fga={}
 
@@ -46,49 +46,21 @@ async def api_setup():
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # Load the ML model
-    print("startup with logstream")
+
     (api_instance,api_client) = await api_setup()
     fga["instance"]=api_instance
     fga["client"]=api_client
     yield
-    # Clean up the ML models and release the resources
-    print("shutdown")
+   
     await api_client.close()
 
 
 app = FastAPI(lifespan=lifespan)
 
-# class Item(BaseModel):
-#     name: str
-#     description: str
-#     price: float
-# class LogData(BaseModel):
-#     date: str
-#     type: str
-#     description: str
-#     client_id: str
-#     client_name: str
-#     ip: str
-#     user_agent: str
-#     user_id: str
-#     log_id: str
-# class LogItem(BaseModel):
-#     log_id: str
-#     #
-#     data: LogData
-#     #data: Model
-#     #{ "log_id": "",  "data": { "date": "2020-01-29T17:26:50.193Z", "type": "sapi", "description": "Create a log stream", "client_id": "", "client_name": "", "ip": "", "user_agent": "", "user_id": "", "log_id": "" }}    
-# class Logs(BaseModel):
-#     logs: list[LogItem]    
-    
+
 @app.get("/")
 async def root():
     return "ok"
-#@app.post("/items/")
-#async def create_item(item: Item):
-#    print("received")
-#    return {"item": item}
 
 @app.post("/log/")
 #async def create_log(logs: Logs):
@@ -127,22 +99,18 @@ async def create_log(request: Request):
                             ),
                         ],
                     ),
-                    #authorization_model_id="1uHxCSuTP0VKPYSnkq1pbb1jeZw",
                 )
                 print (tuple)
                 api_instance = fga["instance"]
                 try:
                     response=await api_instance.write(tuple)
-                    print (response)
                 except:
                     pass
 
             elif (method=="delete" and match_user and statuscode==204):
-                #print ("remove role" +  match_del.group(1))
                 user=match_user.group(1)
                 role=body["roles"][0]
                 print ("del role " + role)
-                #{"user":"user:auth0|6113fe503ee95c0069d42db9","relation":"member","object":"role:rol_99SMnYdy6mILoSFJ"}
                 tuple = WriteRequest(
                     deletes=TupleKeys(
                         tuple_keys=[
@@ -153,14 +121,12 @@ async def create_log(request: Request):
                             ),
                         ],
                     ),
-                    #authorization_model_id="1uHxCSuTP0VKPYSnkq1pbb1jeZw",
                 )
 
                 print (tuple)
                 try:
                     api_instance = fga["instance"]
                     response=await api_instance.write(tuple)
-                    print (response)
                 except:
                     pass
 
@@ -178,15 +144,14 @@ async def create_log(request: Request):
                             ),
                         ],
                     ),
-                    #authorization_model_id="1uHxCSuTP0VKPYSnkq1pbb1jeZw",
                 )
                 print (tuple)
                 api_instance = fga["instance"]
                 try:
                     response=await api_instance.write(tuple)
-                    print (response)
                 except:
                     pass
+
             elif (method=="delete" and match_org and statuscode==204):
                 org=match_org.group(1)
                 user=body["members"][0]
@@ -201,21 +166,18 @@ async def create_log(request: Request):
                             ),
                         ],
                     ),
-                    #authorization_model_id="1uHxCSuTP0VKPYSnkq1pbb1jeZw",
                 )
                 print (tuple)
                 api_instance = fga["instance"]
                 try:
                     response=await api_instance.write(tuple)
-                    print (response)
                 except:
                     pass
+
             elif (method=="post" and match_role_perm and statuscode==201):
                 role=match_role_perm.group(1)
                 perms=body["permissions"]
-                print ("Add perm to role " + role)
                 for perm in perms:
-                    print ("perm " + str(perm))
                     perm_name=perm["permission_name"].replace(":","_")
                     tuple = WriteRequest(
                         writes=TupleKeys(
@@ -227,20 +189,18 @@ async def create_log(request: Request):
                                 ),
                             ],
                         ),
-                        #authorization_model_id="1uHxCSuTP0VKPYSnkq1pbb1jeZw",
                     )
                     print (tuple)
                     api_instance = fga["instance"]
                     try:
                         response=await api_instance.write(tuple)
-                        print (response)
                     except:
                         pass
+
             elif (method=="delete" and match_role_perm and statuscode==204):
                 role=match_role_perm.group(1)
                 perms=body["permissions"]
                 for perm in perms:
-                    print ("remove perm from role " + str(perm))
                     perm_name=perm["permission_name"].replace(":","_")
                     tuple = WriteRequest(
                         deletes=TupleKeys(
@@ -252,7 +212,6 @@ async def create_log(request: Request):
                                 ),
                             ],
                         ),
-                        #authorization_model_id="1uHxCSuTP0VKPYSnkq1pbb1jeZw",
                     )
                     print (tuple)
                     api_instance = fga["instance"]
@@ -279,15 +238,14 @@ async def create_log(request: Request):
                                 ),
                             ],
                         ),
-                        #authorization_model_id="1uHxCSuTP0VKPYSnkq1pbb1jeZw",
                     )
                     print (tuple)
                     api_instance = fga["instance"]
                     try:
                         response=await api_instance.write(tuple)
-                        print (response)
                     except:
                         pass
+
             elif (method=="delete" and match_user_perm and statuscode==204):
                 user=match_user_perm.group(1)
                 perms=body["permissions"]
@@ -304,7 +262,6 @@ async def create_log(request: Request):
                                 ),
                             ],
                         ),
-                        #authorization_model_id="1uHxCSuTP0VKPYSnkq1pbb1jeZw",
                     )
                     print (tuple)
                     api_instance = fga["instance"]
@@ -313,6 +270,7 @@ async def create_log(request: Request):
                         print (response)
                     except:
                         pass
+
     return {"ok"}
 
 if __name__ == "__main__":
