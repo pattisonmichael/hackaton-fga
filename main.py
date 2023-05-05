4e29ee16689513c046b52cae1fccbca06ec08002
Michael Pattison <michael.pattison@auth0.com>
2023-05-05 09:43:33 +0200

    cleanup error handling and rename env.sample

--- main.py
+++ main.py
@@ -78,204 +78,186 @@ async def create_log(request: Request):
         data = json.loads(raw_data.decode("utf-8"))
     except json.JSONDecodeError:
         return {"error": "Invalid JSON data"}
-
-    for log in data["logs"]:
-        if (log["data"]["type"] == "sapi"):
-            print ("SAPI")
-            method=log["data"]["details"]["request"]["method"]
-            path=log["data"]["details"]["request"]["path"]
-            body=log["data"]["details"]["request"]["body"]
-            statuscode=log["data"]["details"]["response"]["statusCode"]
-            match_user = re.match(pattern_user_role,path)
-            match_org = re.match(pattern_user_role_org,path)
-            match_role_perm = re.match(pattern_role_perm,path)
-            match_user_perm = re.match(pattern_user_perm,path)
-            #match_del = re.match(pattern_delete_role,path)
-            if (method=="post" and match_user and statuscode==204):
-                user=unquote(match_user.group(1))
-                role=body["roles"][0]
-                print ("Add role " + role)
-                tuple = WriteRequest(
-                    writes=TupleKeys(
-                        tuple_keys=[
-                            TupleKey(
-                                user=f"user:{user}",
-                                relation="member",
-                                object=f"role:{role}",
-                            ),
-                        ],
-                    ),
-                )
-                print (tuple)
-                api_instance = fga["instance"]
-                try:
-                    response=await api_instance.write(tuple)
-                except:
-                    pass
-
-            elif (method=="delete" and match_user and statuscode==204):
-                user=unquote(match_user.group(1))
-                role=body["roles"][0]
-                print ("del role " + role)
-                tuple = WriteRequest(
-                    deletes=TupleKeys(
-                        tuple_keys=[
-                            TupleKey(
-                                user=f"user:{user}",
-                                relation="member",
-                                object=f"role:{role}",
-                            ),
-                        ],
-                    ),
-                )
-
-                print (tuple)
-                try:
-                    api_instance = fga["instance"]
-                    response=await api_instance.write(tuple)
-                except:
-                    pass
-
-            elif (method=="post" and match_org and statuscode==204):
-                org=match_org.group(1)
-                user=body["members"][0]
-                print ("Add user to org " + org)
-                tuple = WriteRequest(
-                    writes=TupleKeys(
-                        tuple_keys=[
-                            TupleKey(
-                                user=f"user:{user}",
-                                relation="member",
-                                object=f"org:{org}",
-                            ),
-                        ],
-                    ),
-                )
-                print (tuple)
-                api_instance = fga["instance"]
-                try:
-                    response=await api_instance.write(tuple)
-                except:
-                    pass
-
-            elif (method=="delete" and match_org and statuscode==204):
-                org=match_org.group(1)
-                user=body["members"][0]
-                print ("remove user from org " + org)
-                tuple = WriteRequest(
-                    deletes=TupleKeys(
-                        tuple_keys=[
-                            TupleKey(
-                                user=f"user:{user}",
-                                relation="member",
-                                object=f"org:{org}",
-                            ),
-                        ],
-                    ),
-                )
-                print (tuple)
-                api_instance = fga["instance"]
-                try:
-                    response=await api_instance.write(tuple)
-                except:
-                    pass
-
-            elif (method=="post" and match_role_perm and statuscode==201):
-                role=match_role_perm.group(1)
-                perms=body["permissions"]
-                for perm in perms:
-                    perm_name=perm["permission_name"].replace(":","_")
-                    tuple = WriteRequest(
-                        writes=TupleKeys(
-                            tuple_keys=[
-                                TupleKey(
-                                    user=f"role:{role}#member",
-                                    relation="has_permission",
-                                    object=f"permission:{perm_name}",
-                                ),
-                            ],
-                        ),
-                    )
-                    print (tuple)
-                    api_instance = fga["instance"]
-                    try:
-                        response=await api_instance.write(tuple)
-                    except:
-                        pass
-
-            elif (method=="delete" and match_role_perm and statuscode==204):
-                role=match_role_perm.group(1)
-                perms=body["permissions"]
-                for perm in perms:
-                    perm_name=perm["permission_name"].replace(":","_")
-                    tuple = WriteRequest(
-                        deletes=TupleKeys(
-                            tuple_keys=[
-                                TupleKey(
-                                    user=f"role:{role}#member",
-                                    relation="has_permission",
-                                    object=f"permission:{perm_name}",
-                                ),
-                            ],
-                        ),
-                    )
-                    print (tuple)
-                    api_instance = fga["instance"]
-                    try:
-                        response=await api_instance.write(tuple)
-                        print (response)
-                    except:
-                        pass
-
-            elif (method=="post" and match_user_perm and statuscode==201):
-                user=unquote(match_user_perm.group(1))
-                perms=body["permissions"]
-                print ("Add perm to user " + user)
-                for perm in perms:
-                    print ("perm " + str(perm))
-                    perm_name=perm["permission_name"].replace(":","_")
+    try:
+        for log in data["logs"]:
+            if (log["data"]["type"] == "sapi"):
+                print ("SAPI")
+                method=log["data"]["details"]["request"]["method"]
+                path=log["data"]["details"]["request"]["path"]
+                body=log["data"]["details"]["request"]["body"]
+                statuscode=log["data"]["details"]["response"]["statusCode"]
+                match_user = re.match(pattern_user_role,path)
+                match_org = re.match(pattern_user_role_org,path)
+                match_role_perm = re.match(pattern_role_perm,path)
+                match_user_perm = re.match(pattern_user_perm,path)
+                #match_del = re.match(pattern_delete_role,path)
+                if (method=="post" and match_user and statuscode==204):
+                    user=unquote(match_user.group(1))
+                    role=body["roles"][0]
+                    print ("Add role " + role)
                     tuple = WriteRequest(
                         writes=TupleKeys(
                             tuple_keys=[
                                 TupleKey(
                                     user=f"user:{user}",
-                                    relation="has_permission",
-                                    object=f"permission:{perm_name}",
+                                    relation="member",
+                                    object=f"role:{role}",
                                 ),
                             ],
                         ),
                     )
                     print (tuple)
                     api_instance = fga["instance"]
-                    try:
-                        response=await api_instance.write(tuple)
-                    except:
-                        pass
+                    response=await api_instance.write(tuple)
 
-            elif (method=="delete" and match_user_perm and statuscode==204):
-                user=unquote(match_user_perm.group(1))
-                perms=body["permissions"]
-                for perm in perms:
-                    print ("remove perm from user " + str(perm))
-                    perm_name=perm["permission_name"].replace(":","_")
+                elif (method=="delete" and match_user and statuscode==204):
+                    user=unquote(match_user.group(1))
+                    role=body["roles"][0]
+                    print ("del role " + role)
                     tuple = WriteRequest(
                         deletes=TupleKeys(
                             tuple_keys=[
                                 TupleKey(
                                     user=f"user:{user}",
-                                    relation="has_permission",
-                                    object=f"permission:{perm_name}",
+                                    relation="member",
+                                    object=f"role:{role}",
+                                ),
+                            ],
+                        ),
+                    )
+
+                    print (tuple)
+                    api_instance = fga["instance"]
+                    response=await api_instance.write(tuple)
+
+                elif (method=="post" and match_org and statuscode==204):
+                    org=match_org.group(1)
+                    user=body["members"][0]
+                    print ("Add user to org " + org)
+                    tuple = WriteRequest(
+                        writes=TupleKeys(
+                            tuple_keys=[
+                                TupleKey(
+                                    user=f"user:{user}",
+                                    relation="member",
+                                    object=f"org:{org}",
                                 ),
                             ],
                         ),
                     )
                     print (tuple)
                     api_instance = fga["instance"]
-                    try:
+                    response=await api_instance.write(tuple)
+
+                elif (method=="delete" and match_org and statuscode==204):
+                    org=match_org.group(1)
+                    user=body["members"][0]
+                    print ("remove user from org " + org)
+                    tuple = WriteRequest(
+                        deletes=TupleKeys(
+                            tuple_keys=[
+                                TupleKey(
+                                    user=f"user:{user}",
+                                    relation="member",
+                                    object=f"org:{org}",
+                                ),
+                            ],
+                        ),
+                    )
+                    print (tuple)
+                    api_instance = fga["instance"]
+                    response=await api_instance.write(tuple)
+
+                elif (method=="post" and match_role_perm and statuscode==201):
+                    role=match_role_perm.group(1)
+                    perms=body["permissions"]
+                    for perm in perms:
+                        perm_name=perm["permission_name"].replace(":","_")
+                        tuple = WriteRequest(
+                            writes=TupleKeys(
+                                tuple_keys=[
+                                    TupleKey(
+                                        user=f"role:{role}#member",
+                                        relation="has_permission",
+                                        object=f"permission:{perm_name}",
+                                    ),
+                                ],
+                            ),
+                        )
+                        print (tuple)
+                        api_instance = fga["instance"]
                         response=await api_instance.write(tuple)
-                        print (response)
-                    except:
-                        pass
+
+                elif (method=="delete" and match_role_perm and statuscode==204):
+                    role=match_role_perm.group(1)
+                    perms=body["permissions"]
+                    for perm in perms:
+                        perm_name=perm["permission_name"].replace(":","_")
+                        tuple = WriteRequest(
+                            deletes=TupleKeys(
+                                tuple_keys=[
+                                    TupleKey(
+                                        user=f"role:{role}#member",
+                                        relation="has_permission",
+                                        object=f"permission:{perm_name}",
+                                    ),
+                                ],
+                            ),
+                        )
+                        print (tuple)
+                        api_instance = fga["instance"]
+                        response=await api_instance.write(tuple)
+
+                elif (method=="post" and match_user_perm and statuscode==201):
+                    user=unquote(match_user_perm.group(1))
+                    perms=body["permissions"]
+                    print ("Add perm to user " + user)
+                    for perm in perms:
+                        print ("perm " + str(perm))
+                        perm_name=perm["permission_name"].replace(":","_")
+                        tuple = WriteRequest(
+                            writes=TupleKeys(
+                                tuple_keys=[
+                                    TupleKey(
+                                        user=f"user:{user}",
+                                        relation="has_permission",
+                                        object=f"permission:{perm_name}",
+                                    ),
+                                ],
+                            ),
+                        )
+                        print (tuple)
+                        api_instance = fga["instance"]
+                        response=await api_instance.write(tuple)
+
+                elif (method=="delete" and match_user_perm and statuscode==204):
+                    user=unquote(match_user_perm.group(1))
+                    perms=body["permissions"]
+                    for perm in perms:
+                        print ("remove perm from user " + str(perm))
+                        perm_name=perm["permission_name"].replace(":","_")
+                        tuple = WriteRequest(
+                            deletes=TupleKeys(
+                                tuple_keys=[
+                                    TupleKey(
+                                        user=f"user:{user}",
+                                        relation="has_permission",
+                                        object=f"permission:{perm_name}",
+                                    ),
+                                ],
+                            ),
+                        )
+                        print (tuple)
+                        api_instance = fga["instance"]
+                        response=await api_instance.write(tuple)
+    except Exception as e:
+        # Get the type of exception
+        exception_type = type(e).__name__
+
+        # Get the exception message (details)
+        exception_message = str(e)
+
+        print(f"Caught an exception of type {exception_type} with message: {exception_message}")
 
     return {"ok"}
 
